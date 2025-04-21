8d5d1d18e4d63458f9ea603b23c0915d6e70a555
lathoub <bart.de.lathouwer@telenet.be>
2025-04-21 19:16:17 +0200

    added Ready Playing

--- src/choices.js
+++ src/choices.js
@@ -3,6 +3,8 @@ export const outputStatusChoices = []
 outputStatusChoices.push({ id: "play", label: "Play"})
 outputStatusChoices.push({ id: "stop", label: "Stop"})
 outputStatusChoices.push({ id: "pause", label: "Pause"})
+outputStatusChoices.push({ id: "ready", label: "Ready"})
+outputStatusChoices.push({ id: "playing", label: "Playing"})
 
 export const inputStatusChoices = []
 inputStatusChoices.push({ id: "record", label: "Record"})
